3f203dd8c0a40a72e35ca62edb04c01ebfd2a340
Kiara McNulty <kiaramcnulty@gmail.com>
2020-02-19 16:13:46 -0600

    added div stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const msgForm = (ev) => {
     console.log("SEND MESSAGE BUTTON PRESSED");
+    let msgBox = getElementByID("");
+    msgBox.style.display = "none";
 }
 
 let msgBtn = document.querySelector(".page-follow")
