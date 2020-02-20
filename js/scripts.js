6b07f28f6caf1525ec3fa1d26f6a779a3c2ff7fb
wdnnll21 <wesleydonnell2022@u.northwestern.edu>
2020-02-19 20:49:45 -0600

    Prototype 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 const msgForm = (ev) => {
-    let msgBox = document.getElementById("messageform");
-    msgBox.style.display = "block";
+    let msgBox = document.getElementById("form");
+    msgBox.classList.remove("hide");
 }
 
-let msgBtn = document.querySelector(".page-follow")
+const msgSend = (ev) => {
+    let msgBox = document.getElementById("form");
+    msgBox.classList.add("hide");
+    alert("Message Sent!");
+}
+
+let msgBtn = document.querySelector(".page-follow");
 msgBtn.addEventListener("click", msgForm);
+
+let sendBtn = document.getElementById("sendbutton");
+sendBtn.addEventListener("click", msgSend);
