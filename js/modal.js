e3a0244a3e229cd4396e290f43345663e7970767
wdnnll21 <wesleydonnell2022@u.northwestern.edu>
2020-02-26 19:41:24 -0600

    modal help

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,17 @@
-function makeModal(title,category,address,images) {
+function makeModal(info,images,ratio) {
     const modal_template = 
-    `<div class="modal">
-        <button>Close</button>
+    `<div class = "backmodal" onclick = "closeModal()">
+    </div class = "backmodal">
+    <div class="modal" ratioAttr = "${ratio}">
+        <button onclick=closeModal()><i class="fa fa-times"></i></button>
         <div class = "modal-imgs">
-
+        ${images}
         </div>
-        <h1>${data.title}</h1>
-        <h3>${data.category}</h3>
-        <h4>${data.address}</h4>
-        <span class = "like-bar">
-            <div class = "recommend"></div>
-            <div class = "unrecommend"></div>
-        </span>
-        <button>Recommend</button>
-        <button>Discourage</button>
+        ${info}
+        <div class = "like-bar">
+            <div class = "recommend" style = "width:${ratio}%">Like! ${ratio}%</div>
+            <div class = "unrecommend" style = "width:${100-ratio}%">Dislike! ${100-ratio}%</div>
+        </div>
         
         <div class = "tabs">
             <button onclick="onTab(0)">Comments</button>
@@ -22,7 +20,7 @@ function makeModal(title,category,address,images) {
     
         <div class = "tabpane" id = "comments">
             <input type="text">
-            <button>Post</button>
+            <button onclick="addComment()">Post</button>
             <div class="opin">
             </div>
         </div>
@@ -35,40 +33,62 @@ function makeModal(title,category,address,images) {
     </div>`;
 
     document.querySelector("body").innerHTML += modal_template;
-
-    images = document.querySelector(".modal-imgs")
-    for(image of images) {
-        images.innerHTML += `<img src="${image}">`
-    }
-
+    onTab(0);
 }
 
 function onTab(id) {
     if(id===0) {
         document.querySelector("#comments").style = ""
         document.querySelector("#questions").style = "display:none;"
+        document.querySelector(".tabs").children[0].classList.add("selected")
+        document.querySelector(".tabs").children[1].classList.remove("selected")
     }
     if(id===1) {
         document.querySelector("#questions").style = ""
         document.querySelector("#comments").style = "display:none;"
+        document.querySelector(".tabs").children[1].classList.add("selected")
+        document.querySelector(".tabs").children[0].classList.remove("selected")
     }
 }
 
 function likeIt() {
+     
+}
+
+function dislikeIt() {
 
 }
 
-function addComment(comment) {
+function closeModal() {
+    elem = document.querySelector(".modal");
+    elem.parentNode.removeChild(elem);
+    elemtwo = document.querySelector(".backmodal");
+    elemtwo.parentNode.removeChild(elemtwo);
+}
+
+function addComment() {
+    const comment = document.querySelector("#comments > input").value;
     const commentcard = 
     `<div class = "comment-card">
-        <h3>Kiara McNulty<h3>
+        <h3>Kiara<h3>
         <p>${comment}</p>
     </div>`;
     document.querySelector(".opin").innerHTML += commentcard;
+    document.querySelector("#comments > input").value = "";
 }
 
 for(card of document.querySelectorAll(".postcard")) {
     card.addEventListener("click",function(ev) {
-        ev.currentTarget;
+        a = ev.currentTarget.querySelectorAll("h2,h3,h4");
+        b = ev.currentTarget.querySelectorAll("img");
+        texts = ``
+        images = ``
+        for(text of a) {
+            texts+=text.outerHTML;
+        }
+        for(image of b) {
+            images += image.outerHTML;
+        }
+        makeModal(texts,images,71)
     });
 }
